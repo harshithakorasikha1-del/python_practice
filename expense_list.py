e1770530a151512e4d60f40d11cb572f8c7d3021
Harshitha K <harshithakorasikha1@gmail.com>
2026-02-01 17:55:18 +0530

    CLI Expense Manager with File Persistence

---
+++ expense_list.py
@@ -0,0 +1,68 @@
+def expenses():
+    expense_list = []
+
+    with open("expenseslist.txt", "a+") as v:
+        v.seek(0)
+        lines=v.readlines()
+
+        for line in lines:
+            line=line.strip()
+            if "," not in line:
+                continue
+
+            parts=line.split(",")
+            try:
+                expense={
+
+                    "amount": float(parts[0]),
+                    "category": (parts[1]),
+                    "note": (parts[2])
+                }
+                expense_list.append(expense)
+            except ValueError:
+                continue
+
+    with open("expenseslist.txt", "w") as f:
+        while True:
+            print("\n1.Add expense")
+            print("2.Exit")
+
+            choice = input("Enter choice:")
+
+            if choice == "1":
+                try:
+                    amount = float(input("Enter the amount:"))
+                
+                except ValueError:
+                    print("Please enter valid number")
+                    continue
+
+                category = input("Enter the category:")
+                note = input("Enter the note:")
+
+                expenses = {"amount":amount,
+                       "category":category,
+                       "note":note
+                      }
+                expense_list.append(expenses)
+
+            elif choice == "2":
+                break
+
+            else:
+                print("Invalid choice. Please enter 1 or 2")
+
+        total = 0
+
+        print("\nAll Expenses:")
+        for exp in expense_list:
+            f.write(f"{exp['amount']},{exp['category']},{exp['note']}\n")
+            total += exp['amount']
+
+    with open("expenseslist.txt","r") as d:
+        print(d.read())
+              
+    print(f"Number of expenses: {len(expense_list)}")
+    print(f"Total amount spent:{total:.2f}\n")
+    
+expenses()
